26b3ec98cbeda994a2750816781baebdcd3899e6
Berryice <110242962+Berryice@users.noreply.github.com>
2026-03-10 04:17:04 +0500

    initialization of the project
    creating a project for further work

---
+++ uchebnaya-praktika/src/main/java/com/practice/uchebnayapraktika/UchebnayaPraktikaApplication.java
@@ -0,0 +1,13 @@
+package com.practice.uchebnayapraktika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UchebnayaPraktikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UchebnayaPraktikaApplication.class, args);
+	}
+
+}
